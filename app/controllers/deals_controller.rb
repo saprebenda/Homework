33cd77d2b96b5d41d1165a4a7966a2bf1c942ae6
Shane A. Prebenda <saprebenda@yahoo.com>
2013-11-12 13:18:48 -0500

    deals.each

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -37,6 +37,7 @@ class DealsController < ApplicationController
       @deals_high = Deal.high_price
       @deals_med = Deal.med_price
       @deals_low = Deal.low_price
+      @deals = Deal.all
     else
       @deals = []
     end
