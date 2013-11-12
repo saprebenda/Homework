e723607c3bc8a6389124f107f6cebaa0c532641b
Shane A. Prebenda <saprebenda@yahoo.com>
2013-11-12 12:54:36 -0500

    changes scopes up

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,8 +1,8 @@
 class Deal < ActiveRecord::Base
-  belongs_to :User
-  attr_accessible :Object_Name, :Price, :User_id
   scope :Object_sort, -> { order(:Object_Name)}
-  scope :high_price, -> { where('Price = ?', 500)}
+  scope :high_price, -> { where('Price > ?', 500)}
   scope :med_price, -> { where('Price >= ? AND Price <= ?', 300, 500)}
   scope :low_price, -> { where('Price < ?', 300)}
+  belongs_to :User
+  attr_accessible :Object_Name, :Price, :User_id
 end
