81763e63794b6b6c50b96e1bb24290ecf88e1909
Shane A. Prebenda <saprebenda@yahoo.com>
2013-11-12 23:35:20 -0500

    changed Prices to lowercase

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,8 +1,8 @@
 class Deal < ActiveRecord::Base
   scope :Object_sort, -> { order(:Object_Name)}
-  scope :high_price, -> { where('Price > ?', 500)}
-  scope :med_price, -> { where('Price >= ? AND Price <= ?', 300, 500)}
-  scope :low_price, -> { where('Price < ?', 300)}
+  scope :high_price, -> { where('price > ?', 500)}
+  scope :med_price, -> { where('price >= ? AND price <= ?', 300, 500)}
+  scope :low_price, -> { where('price < ?', 300)}
   belongs_to :User
-  attr_accessible :Object_Name, :Price, :User_id
+  attr_accessible :Object_Name, :price, :User_id
 end
