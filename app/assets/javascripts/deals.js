dd15a975de2851336ceeec5c635b902cc40badb4
Shane A. Prebenda <saprebenda@yahoo.com>
2013-11-18 22:18:28 -0500

    high med low background colors

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -1,5 +1,17 @@
 $(function(){
-	$("#title").hover(function(){
-		$("#title").css("background-color","red");
+	$(".high").hover(function(e){
+		$(e.target).closest(".high").css("background-color","red");
+	}, function(){
+		$(".high").css("background-color","");
+	});	
+	$(".med").hover(function(e){
+		$(e.target).closest(".med").css("background-color","blue");
+	}, function(){
+		$(".med").css("background-color","");
+	});	
+	$(".low").hover(function(e){
+		$(e.target).closest(".low").css("background-color","green");
+	}, function(){
+		$(".low").css("background-color", "")
 	});
 });
