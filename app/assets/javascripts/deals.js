9308fdcfb1c1d44c9112753bf75652243717353a
Shane A. Prebenda <saprebenda@yahoo.com>
2013-11-19 00:39:22 -0500

    switched colors

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -5,12 +5,12 @@ $(function(){
 		$(".high").css("background-color","");
 	});	
 	$(".med").hover(function(e){
-		$(e.target).closest(".med").css("background-color","yellow");
+		$(e.target).closest(".med").css("background-color","green");
 	}, function(){
 		$(".med").css("background-color","");
 	});	
 	$(".low").hover(function(e){
-		$(e.target).closest(".low").css("background-color","green");
+		$(e.target).closest(".low").css("background-color","yellow");
 	}, function(){
 		$(".low").css("background-color", "")
 	});
